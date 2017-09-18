449dc759434ab1a2cbd971039d1ece09d0dbf9fd
Christine Tran <christinetran825@gmail.com>
2017-09-18 03:02:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,26 +11,26 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Build a Diner", "games", 45333, 2017-10-9, 2017-11-18);
 
 --users
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
-INSERT INTO users (name, age) VALUES ();
+INSERT INTO users (name, age) VALUES ("John", 22);
+INSERT INTO users (name, age) VALUES ("Pam", 34);
+INSERT INTO users (name, age) VALUES ("Michael", 14);
+INSERT INTO users (name, age) VALUES ("Jim", 26);
+INSERT INTO users (name, age) VALUES ("Dale", 77);
+INSERT INTO users (name, age) VALUES ("Barb", 45
+INSERT INTO users (name, age) VALUES ("Sarah", 89);
+INSERT INTO users (name, age) VALUES ("Steve", 15);
+INSERT INTO users (name, age) VALUES ("Lloyd", 82);
+INSERT INTO users (name, age) VALUES ("Alan", 22);
+INSERT INTO users (name, age) VALUES ("George", 10);
+INSERT INTO users (name, age) VALUES ("Peter", 4);
+INSERT INTO users (name, age) VALUES ("Molly", 25);
+INSERT INTO users (name, age) VALUES ("Lynn", 12);
+INSERT INTO users (name, age) VALUES ("Elaine", 65);
+INSERT INTO users (name, age) VALUES ("Cece", 45);
+INSERT INTO users (name, age) VALUES ("Jessie", 33);
+INSERT INTO users (name, age) VALUES ("Nick", 23
+INSERT INTO users (name, age) VALUES ("Schmit", 5);
+INSERT INTO users (name, age) VALUES ("Winston", 12);
 
 --pledges
 INSERT INTO pledge (amount, user_id, project_id) VALUES ();
