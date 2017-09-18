d39360bbef112493d070565d3146c098445cbe38
Christine Tran <christinetran825@gmail.com>
2017-09-18 03:15:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,36 +1,37 @@
 --projects
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Red Wagon", "art", 25000, "2017-12-15", "2018-1-15");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cement Planters", "crafts", 200, "2017-11-11", "2017-12-15");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Seed Museum", "food", 125000, "2017-2-15", "2017-3-25");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The World Within", "journalism", 1000, "2017-7-22", "2017-8-22");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Samurai Statue", "art", 20000, "2018-2-10", "2018-4-22");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Little Bungalow", "publishing", 5000, "2018-5-15", "2018-6-15");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Jet Propel Backpack", "technology", 67000, "2018-8-9", "2019-1-5");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Troll Monument", "art", 300000, "2017-12-15", "2018-4-15");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Clean the Ocean", "technology", 687000000, "2018-1-5", "2019-1-5");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Build a Diner", "games", 45333, "2017-10-9", "2017-11-18");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+(1, "Red Wagon", "art", 25000, "2017-12-15", "2018-1-15");
+(2, "Cement Planters", "crafts", 200, "2017-11-11", "2017-12-15");
+(3, "Seed Museum", "food", 125000, "2017-2-15", "2017-3-25");
+(4, "The World Within", "journalism", 1000, "2017-7-22", "2017-8-22");
+(5, "Samurai Statue", "art", 20000, "2018-2-10", "2018-4-22");
+(6, "The Little Bungalow", "publishing", 5000, "2018-5-15", "2018-6-15");
+(7, "Jet Propel Backpack", "technology", 67000, "2018-8-9", "2019-1-5");
+(8, "Troll Monument", "art", 300000, "2017-12-15", "2018-4-15");
+(9, "Clean the Ocean", "technology", 687000000, "2018-1-5", "2019-1-5");
+(10, "Build a Diner", "games", 45333, "2017-10-9", "2017-11-18");
 
 --users
-INSERT INTO users (name, age) VALUES ("John", 22);
-INSERT INTO users (name, age) VALUES ("Pam", 34);
-INSERT INTO users (name, age) VALUES ("Michael", 14);
-INSERT INTO users (name, age) VALUES ("Jim", 26);
-INSERT INTO users (name, age) VALUES ("Dale", 77);
-INSERT INTO users (name, age) VALUES ("Barb", 45
-INSERT INTO users (name, age) VALUES ("Sarah", 89);
-INSERT INTO users (name, age) VALUES ("Steve", 15);
-INSERT INTO users (name, age) VALUES ("Lloyd", 82);
-INSERT INTO users (name, age) VALUES ("Alan", 22);
-INSERT INTO users (name, age) VALUES ("George", 10);
-INSERT INTO users (name, age) VALUES ("Peter", 4);
-INSERT INTO users (name, age) VALUES ("Molly", 25);
-INSERT INTO users (name, age) VALUES ("Lynn", 12);
-INSERT INTO users (name, age) VALUES ("Elaine", 65);
-INSERT INTO users (name, age) VALUES ("Cece", 45);
-INSERT INTO users (name, age) VALUES ("Jessie", 33);
-INSERT INTO users (name, age) VALUES ("Nick", 23
-INSERT INTO users (name, age) VALUES ("Schmit", 5);
-INSERT INTO users (name, age) VALUES ("Winston", 12);
+INSERT INTO users (id, name, age) VALUES ("John", 22);
+INSERT INTO users (id, name, age) VALUES ("Pam", 34);
+INSERT INTO users (id, name, age) VALUES ("Michael", 14);
+INSERT INTO users (id, name, age) VALUES ("Jim", 26);
+INSERT INTO users (id, name, age) VALUES ("Dale", 77);
+INSERT INTO users (id, name, age) VALUES ("Barb", 45
+INSERT INTO users (id, name, age) VALUES ("Sarah", 89);
+INSERT INTO users (id, name, age) VALUES ("Steve", 15);
+INSERT INTO users (id, name, age) VALUES ("Lloyd", 82);
+INSERT INTO users (id, name, age) VALUES ("Alan", 22);
+INSERT INTO users (id, name, age) VALUES ("George", 10);
+INSERT INTO users (id, name, age) VALUES ("Peter", 4);
+INSERT INTO users (id, name, age) VALUES ("Molly", 25);
+INSERT INTO users (id, name, age) VALUES ("Lynn", 12);
+INSERT INTO users (id, name, age) VALUES ("Elaine", 65);
+INSERT INTO users (id, name, age) VALUES ("Cece", 45);
+INSERT INTO users (id, name, age) VALUES ("Jessie", 33);
+INSERT INTO users (id, name, age) VALUES ("Nick", 23
+INSERT INTO users (id, name, age) VALUES ("Schmit", 5);
+INSERT INTO users (id, name, age) VALUES ("Winston", 12);
 
 --pledges
 INSERT INTO pledge (amount, user_id, project_id) VALUES (10.00, 1, 2);
